9d147efc5a049041a1a32c0f6765f94265c3cf17
Lalit Badoni <badonilalit@gmail.com>
2018-06-08 10:04:47 +0530

    My Project Setup Completed Successfully.

---
+++ onlishopping/src/main/java/com/lalit/onlishopping/backend/App.java
@@ -0,0 +1,13 @@
+package com.lalit.onlishopping.backend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
